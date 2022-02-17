5affdcf712994e2dd37a9385c32a7771e21cc0bc
KerimCanD <kcandagtekin@gmail.com>
2022-02-18 00:08:44 +0300

    add foundation cdn

---
+++ app/controllers/time_tracks_controller.rb
@@ -0,0 +1,32 @@
+class TimeTracksController < ApplicationController
+  before_action :set_time_track, only: %i[ show edit update destroy ]
+
+  def index
+    @time_tracks = TimeTrack.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  private
+
+  def set_time_track
+    @time_track = TimeTrack.find(params[:id])
+  end
+
+  def time_track_params
+    params.require(:time_track).permit(:time_started, :time_ended, :description, :type)
+  end
+end
